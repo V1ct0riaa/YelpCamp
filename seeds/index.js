1268862c9f9d14670a79ff8e967d958359f757ac
Natanael Pascal <npascal70@gmail.com>
2026-03-16 17:49:00 +0700

    feat:finished on basic styling with bootstrap

--- seeds/index.js
+++ seeds/index.js
@@ -23,9 +23,13 @@ const seedDB = async () => {
     await Campground.deleteMany({});
     for (let i = 0; i < 50; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor((Math.random() * 30) + 10)
         const camp = new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: `https://picsum.photos/seed/yelpcamp-${i}/400/300`,
+            description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Incidunt dignissimos  fugiat aliquam atque ipsa tenetur est recusandae nostrum sint? Rem, adipisci? Doloribus non ipsum omnis, quas dicta doloremque impedit culpa',
+            price
         })
         await camp.save();
     }
